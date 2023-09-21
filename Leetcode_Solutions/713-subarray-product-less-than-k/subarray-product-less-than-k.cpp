6bc70c2209d5caa9436b46487e6254993af7e0cd
RYAN REGO <105663938+RYAN-REGO@users.noreply.github.com>
2023-09-21 19:39:08 +0530

    Time: 69 ms (27.83%) | Memory: 61.3 MB (23.61%) - LeetSync

--- Leetcode_Solutions/713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
+++ Leetcode_Solutions/713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
@@ -1,21 +1,21 @@
+//Doubt : incrementing count
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        int n = nums.size();
-        int prod = 1;
-        int cnt = 0;
-        for(int i = 0; i < n; i++)
-        {
-        //if(nums[i] < k) cnt++;
-            for(int j = i; j < n; j++)
-            {
-                prod *= nums[j];
-                if(prod < k) cnt++;
-                //if prod goes greater than k no need of going further
-                else break;
-            }
-            prod = 1;
+        if(k<=1){
+            return 0;
         }
-        return cnt;
+        int prod=1;
+        int count=0;
+        int i=0;
+        for(int j=0;j<nums.size();j++){
+            prod*=nums[j];
+            while(prod>=k){
+                prod/=nums[i];
+                i++;
+            }
+            count+=j-i+1;
+        }
+        return count;
     }
 };
