710988f81b330e258dc9f321a8b74330e66fa177
RYAN REGO <105663938+RYAN-REGO@users.noreply.github.com>
2023-09-24 15:06:55 +0530

    Time: 76 ms (6.64%) | Memory: 32.2 MB (10.48%) - LeetSync

--- Leetcode_Solutions/1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
+++ Leetcode_Solutions/1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
@@ -18,18 +18,3 @@ public:
     }
 };
 
-	int subarraysDivByK(vector<int>& nums, int k) {
-  map<int,int>map;
-map[0]=1;
-int sum=0,count=0;
-for (int i=0;i<nums.size();i++){
-    sum =sum+nums[i];
-    int remainder=(sum%k);
-    if(remainder<0){
-    remainder=remainder+k;
-    }
-    count=count+map[remainder];
-    map[remainder]++;
-}
-return count;
-}
