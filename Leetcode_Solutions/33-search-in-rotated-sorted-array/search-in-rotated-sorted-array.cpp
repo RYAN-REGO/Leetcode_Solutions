406b5c8e4ecd6c54fa8a3fa82370c772c09b7cad
RYAN REGO <105663938+RYAN-REGO@users.noreply.github.com>
2023-09-25 02:25:23 +0530

    Time: 3 ms (65.11%) | Memory: 11.3 MB (6.46%) - LeetSync

---
+++ Leetcode_Solutions/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+
+        while(low <= high)
+        {
+            int mid = (low + high) / 2;
+            if(nums[mid] == target) return mid;
+
+            if(nums[low] <= nums[mid])
+            {
+                //here we can say that the left part is sorted
+                if(nums[low] <= target && nums[mid] >= target)
+                {
+                    //element exists within bounds
+                    high = mid-1;
+                }
+                else
+                {   
+                    low = mid+1;
+                }
+            }
+            else
+            {
+                //here we can say that the right part is sorted
+                if(nums[mid] <= target && nums[high] >= target)
+                {
+                    low = mid+1;
+                }
+                else
+                {
+                    high = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};
