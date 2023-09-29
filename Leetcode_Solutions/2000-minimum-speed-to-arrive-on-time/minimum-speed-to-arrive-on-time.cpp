c36fbf9f1dbba0f5ce3ad6ec7e340837bae8ec9f
RYAN REGO <105663938+RYAN-REGO@users.noreply.github.com>
2023-09-30 01:36:33 +0530

    Time: 243 ms (36.78%) | Memory: 101.9 MB (6.03%) - LeetSync

---
+++ Leetcode_Solutions/2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& dist, int speed, double hour)
+    {
+        double ans = 0;
+        for(int i = 0; i < dist.size(); i++)
+        {
+            //both dist and speed are int so the result will also be int
+            //hence to convert it into double mul by 1.0
+            ///OR double d = double(dist[i]) / double(speed);
+            double d = dist[i] * 1.0 / speed;
+            if(i == dist.size() - 1)
+            {
+                ans += d;
+            }
+            else
+            {
+                ans = ans + ceil(d);
+            }
+            if(ans > hour) return false;
+        }
+        if(ans <= hour)
+        {
+            //possible to reach within the time
+            return true;
+        }
+        else
+        {
+            //not possible to reach within time
+            return false;
+        }
+    }
+    int minSpeedOnTime(vector<int>& dist, double hour) {
+        
+        int n = dist.size();
+        if(hour < n - 1) return -1;
+        int low = 1;
+        int high = 1e7;
+        //for(int i = 0; i < n; i++)
+        //{
+           //high = max(high, dist[i]);
+        //}
+        int ans = -1;
+        //we have the low and the high
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            //find the total hrs at speed mid
+            if(isPossible(dist, mid, hour))
+            {
+                //if possible then it can be our answer
+                //but try for evan smaller
+                ans = mid;
+                high = mid-1;
+            }
+            else
+            {
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+};
