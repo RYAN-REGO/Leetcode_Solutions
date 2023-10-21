d470b451fddf8e9d399286b46db13af171ff9005
RYAN REGO <105663938+RYAN-REGO@users.noreply.github.com>
2023-10-21 19:01:34 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (26.73%) - LeetSync

---
+++ Leetcode_Solutions/114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -0,0 +1,25 @@
+
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if(!root) return;
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while(!st.empty())
+        {
+            TreeNode* curr = st.top();
+            st.pop();
+
+            if(curr->right) st.push(curr->right);
+            if(curr->left) st.push(curr->left);
+
+            if(!st.empty())
+            {
+                curr->right = st.top();
+            }
+            curr->left = NULL;
+        }
+        return;
+    }
+};
